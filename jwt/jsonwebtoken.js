0fa1c3c8c753acd5303f9f7f448e0f2f96020e44
姚正亮 <l_yzliang@163.com>
2018-05-31 23:16:15 +0800

    修改

--- jwt/jsonwebtoken.js
+++ jwt/jsonwebtoken.js
@@ -15,5 +15,15 @@ app.post('/api/login', (req, res) => {
   res.json({"status": "ok", "token": token, "body": body});
 })
 
+app.get('/api/jiema', (req, res) => {
+  console.log(req.headers.authorization);
+  jwt.verify(token, secret, function (err, decoded) {
+    if (!err){
+          console.log(decoded.name);  //会输出123，如果过了60秒，则有错误。
+          res.json();
+     }
+})
+})
+
 app.use('/frontend', express.static('./html'));
 app.listen(3000);
