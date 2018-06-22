be3411edb2eb786c0a6b5c6bd73488186b99a030
姚正亮 <l_yzliang@163.com>
2018-06-22 11:04:48 +0800

    练习上传七牛

--- upload/app.js
+++ upload/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var formidable  = require('formidable');
 var path = require("path");
 
+var fs = require('fs');
+var upload = require('./upload');
+
 var app = express();
 
 // 单图上传
@@ -13,11 +16,27 @@ app.post('/upload', function(req, res){
     form.parse(req, function(err, fields, files) {
       var extname = path.extname(files.file.name);
       var oldpath = __dirname + "/" + files.file.path;
-      var newpath = __dirname + "/img/" + (new Date()).getTime() + extname;
-      // //改名
-      fs.rename(oldpath,newpath,function(err){
-        res.json(files);
-      });
+      // var newpath = __dirname + "/img/" + (new Date()).getTime() + extname;
+      
+      var key=(new Date()).getTime() + extname;
+      // 文件上传
+      function cd(respErr,respBody, respInfo) {
+        if (respErr) {
+          throw respErr;
+        }
+        if (respInfo.statusCode == 200) {
+          console.log(respBody);
+          fs.unlink(oldpath, (err) => {
+            if (err) throw err;
+            console.log('成功删除 /tmp/hello');
+          });
+        } else {
+          console.log(respInfo.statusCode);
+          console.log(respBody);
+        }
+        res.json({"status": "ok"});
+      };
+      upload(key, oldpath, cd);
     });
 });
 
